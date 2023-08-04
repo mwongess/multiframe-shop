e438bbe46334fbff51b2e778725caa03842a76b4
mwongess <amosmwongelah@gmail.com>
2023-08-04 23:28:04 +0300

    feat: generate products page and readmore page

---
+++ next-shop/src/app/components/Product.tsx
@@ -0,0 +1,17 @@
+const Product = () => {
+    return (
+        <div className="border ">
+            <div>
+                <img src="" alt="img" />
+            </div>
+            <h1>Trouser</h1>
+            <p >Short Description</p>
+            <div className="flex justify-between">
+                <p className="">Price</p>
+                <p>Rating</p>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
