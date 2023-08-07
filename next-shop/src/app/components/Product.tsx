c1002e1dd6e256b1535aa4835125b0908d86244d
mwongess <amosmwongelah@gmail.com>
2023-08-08 02:35:35 +0300

    feat: add readmore page

--- next-shop/src/app/components/Product.tsx
+++ next-shop/src/app/components/Product.tsx
@@ -3,29 +3,31 @@
 import Link from "next/link";
 
 const Product = ({
-  product,
+    product,
 }: {
-  product: { image: string; name: string; description: string; price: number };
+    product: { image: string; name: string; description: string; price: number };
 }) => {
-  return (
-    <Link href="/readmore/1" className="bg-[#fefefe] rounded-lg cursor-pointer">
-      <div className="h-40 ">
-        <img
-          className="w-full h-full object-cover rounded-t-lg"
-          src="https://image.kilimall.com/kenya/shop/store/goods/6070/2023/03/167824720799437190b023c91422f83bddcc292a43925_240.jpg.webp"
-          alt="img"
-        />
-      </div>
-      <div className="p-3">
-        <h1>{product.name}</h1>
-        <p className="font-bold">{product.description}</p>
-        <div className="flex justify-between">
-          <p className="text-[#2a977d] font-bold">${product.price}</p>
-          {/* <p>{product.}</p> */}
-        </div>
-      </div>
-    </Link>
-  );
+    return (
+        <Link href="/readmore/1" className="bg-[#fefefe] rounded-lg cursor-pointer">
+            <div className=" ">
+                <img
+                    className="w-full h-full object-cover rounded-t-lg"
+                    src="https://image.kilimall.com/kenya/shop/store/goods/6070/2023/03/167824720799437190b023c91422f83bddcc292a43925_240.jpg.webp"
+                    alt="img"
+                />
+            </div>
+            <div className="p-3">
+                <h1>{product.name}</h1>
+                <p className="font-bold">{product.description}</p>
+                <div className="flex justify-between">
+                    <p className="text-[#fd6141] font-bold text-lg">${product.price}</p>
+                   
+
+
+                </div>
+            </div>
+        </Link>
+    );
 };
 
 export default Product;
--- next-shop/src/app/readmore/[id]/page.tsx
+++ next-shop/src/app/readmore/[id]/page.tsx
@@ -1,11 +1,37 @@
-"use client"
+"use client";
 
-const Readmore = ({params}:{params: any}) => {
+const Readmore = ({ params }: { params: any }) => {
   return (
-    <div>
-        <p>{params.id}</p>
+    <div className="flex border-white justify-center p-8 gap-8">
+      <div className="w-[30%]">
+        <img
+          className="w-full h-full object-cover"
+          src="https://image.kilimall.com/kenya/shop/store/goods/6070/2023/03/1678247207784c49f20bca4f04609bcdb898322ebba7f_720.jpg.webp"
+          alt="img"
+        />
+      </div>
+      <div className="w-[40%] border-b border-white">
+        <h1>Description</h1>
+        <div>
+          <p>Choose size</p>
+          <div>
+            <p>S</p>
+            <p>M</p>
+            <p>L</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col bg-[#fefefe] p-3 gap-3 w-[20%]">
+        <div>
+          <h1>Total price</h1>
+        </div>
+        <button className="bg-[#fd6141] text-white p-3">Buy Now</button>
+        <button className="border border-[#fd6141] text-[#fd6141] p-3">
+          Add To Bag
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Readmore
+export default Readmore;
