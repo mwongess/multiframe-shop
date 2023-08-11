35938c5bb548fa592255ac336ff32fba3d9d164d
mwongess <amosmwongelah@gmail.com>
2023-08-11 12:32:38 +0300

    add softr.io shape on auth layout

--- next-shop/src/app/auth/components/Swiper.tsx
+++ next-shop/src/app/auth/components/Swiper.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Swiper = () => {
   return (
-    <div className='hidden sm:flex flex-col align-center justify-center'>
-        <img className='h-[15rem] object-contain' src="/images.jpeg" alt="" />
+    <div className='w-[60%] hidden sm:flex flex-col align-center justify-center bg-[url(/graph.png)] bg-center bg-no-repeat px-4'>
+        <img className='h-[15rem] object-contain' src="/auth.png" alt="" />
     </div>
   )
 }
--- next-shop/src/app/auth/layout.tsx
+++ next-shop/src/app/auth/layout.tsx
@@ -3,9 +3,11 @@ import Swiper from "./components/Swiper";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="p-3 min-h-screen sm:flex items-center justify-center gap-[10rem]">
-      {children}
-      <Swiper />
+    <div className="p-3 min-h-screen sm:flex items-center justify-center">
+      <div className="flex gap-[10rem]">
+        {children}
+        <Swiper />
+      </div>
     </div>
   );
 };
