926f785da28b24941b72223752244bfc04362510
mwongess <amosmwongelah@gmail.com>
2023-08-12 14:12:37 +0300

    wrap up product details ui

--- next-shop/src/components/QuantityAdjuster.tsx
+++ next-shop/src/components/QuantityAdjuster.tsx
@@ -1,6 +1,13 @@
 const QuantityAdjuster = () => {
   return (
-    <div>QuantityAdjuster</div>
+    <div className="flex justify-between">
+        <div className="flex justify-around p-2 border rounded-lg w-[50%]">
+            <p className="cursor-pointer font-bold">-</p>
+            <p className="cursor-pointer font-bold">1</p>
+            <p className="cursor-pointer font-bold">+</p>
+        </div>
+        <p>Stock: <span className="font-bold">459</span></p>
+    </div>
   )
 }
 
