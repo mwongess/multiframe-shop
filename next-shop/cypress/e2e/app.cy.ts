27491e84ccc8a794c8e26c2959746756bf42d83e
mwongess <amosmwongelah@gmail.com>
2023-08-16 13:17:25 +0300

    test: navigate to login and actually login

--- next-shop/cypress/e2e/app.cy.ts
+++ next-shop/cypress/e2e/app.cy.ts
@@ -1,5 +1,16 @@
 describe('Navigation', () => {
-    it('should navigate to products page', () => {
-      cy.visit('/')
+    it('should navigate to login page and perform login', () => {
+      // Visit the login page
+      cy.visit('/auth/login')
+  
+      // Type email and password
+      cy.get('.email').type('nextshop@test.app')
+      cy.get('.pass').type('nextshop')
+  
+      // Click the login button
+      cy.contains('login').click()
+  
+      // You can add assertions here to verify successful login if needed
     })
   })
+  
