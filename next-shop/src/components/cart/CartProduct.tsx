4fac5acb870455223351756a9260340c94e705b0
mwongess <amosmwongelah@gmail.com>
2023-08-21 22:04:28 +0300

    feat: update cart

--- next-shop/src/components/cart/CartProduct.tsx
+++ next-shop/src/components/cart/CartProduct.tsx
@@ -1,6 +1,15 @@
+import ProductImage from "./ProductImage"
+
 const CartProduct = () => {
   return (
-    <div>CartProduct</div>
+    <div>
+      <ProductImage/>
+      <div>
+        <p>Lorem ipsum dolor sit.</p>
+        <p>Delivery in 2 days</p>
+        <button>Remove</button>
+      </div>
+    </div>
   )
 }
 
